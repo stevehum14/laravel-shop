906ecb425d644a2c0f9e33a0821a3851ba9b412b
洪学臣 <stevehum14@gmail.com>
2021-03-16 20:43:17 +0800

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -2,8 +2,8 @@
 
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
+        'app_id'         => '2016092000554679',
+        'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7TxbvO7m7vmsuxuW+2vp4adxIAYd9uj8Tc2FOU6FjQZOepBsYV0lXe8zo32gjdqI81lJ7R7EBxCNu+rkGN+aedQGppeBXjMd5dfLZvajE1JowwnW2F4RZBLMHGk8c4HGgIQDkT5LwiB0urVVjbMZY9osYvIZfi7BXQJybJaoa9b2dcmhw+Lut55YUHm91lg3AATHk4xokRnLfx0di60c0Y/wONEck3mJ7hRRo75+CUhKxXWzkBK/DsQMhOsRG2BPPq0lDW5o6UPIh8cAwAIzfuVJZSOqjC7QNS3wDfzbEVSyyZ26LTvT2ofEZnvIk/oRp+j/gK3AjsHUfC20pPWPyQIDAQAB',
         'private_key'    => '',
         'log'            => [
             'file' => storage_path('logs/alipay.log'),
