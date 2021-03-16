3f59b621b56ed4aea1258df7a089f66dfd544d63
洪学臣 <stevehum14@gmail.com>
2021-03-16 15:36:55 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,7 @@ Route::group(['middleware'=>['auth']],function(){
     //购物车-删除商品
     Route::delete('cart/{sku}','CartController@remove')->name('cart.remove');
 
+    Route::post('orders','OrdersController@store')->name('orders.store');
+
 });
 
