48d54519845bbc754523ec3c22fa5b589ef6abcf
洪学臣 <stevehum14@gmail.com>
2021-03-16 19:27:26 +0800

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,29 +5,39 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+    protected $cartService;
+    // 利用 laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
+//        $user = $request->user();
         $skuId = $request->input('sku_id');
         $amount = $request->input('amount');
+        $this->cartService->add($skuId,$amount);
+        return [];
 
-        //从数据库中查询该商品是否已经在购物车中
-        if($cart = $user->cartItems()->where('product_sku_id',$skuId)->first()){
-            //如果存在则直接叠加商品数量
-            $cart->update([
-                'amount'=>$cart->amount + $amount,
-            ]);
-        }else{
-            //否则创建一个新的购物车记录
-            $cart = new CartItem(['amount'=>$amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+//        //从数据库中查询该商品是否已经在购物车中
+//        if($cart = $user->cartItems()->where('product_sku_id',$skuId)->first()){
+//            //如果存在则直接叠加商品数量
+//            $cart->update([
+//                'amount'=>$cart->amount + $amount,
+//            ]);
+//        }else{
+//            //否则创建一个新的购物车记录
+//            $cart = new CartItem(['amount'=>$amount]);
+//            $cart->user()->associate($user);
+//            $cart->productSku()->associate($skuId);
+//            $cart->save();
+//        }
     }
 
     /*
@@ -35,7 +45,8 @@ class CartController extends Controller
      */
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+//        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at','desc')->get();
         return view('cart.index',['cartItems'=>$cartItems,'addresses'=>$addresses]);
     }
