c5039b0720eec1c52b043f45463c9413fde8ae43
洪学臣 <stevehum14@gmail.com>
2021-03-20 13:38:49 +0800

    管理后台 - 同意退款 -微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -75,4 +75,5 @@ Route::group(['middleware'=>['auth']],function(){
 
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/wechat/refund_notify','PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
