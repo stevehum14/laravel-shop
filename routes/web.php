cd9f4e1da509514d08bb91acd7917dff3317e8f1
洪学臣 <stevehum14@gmail.com>
2021-03-24 11:56:49 +0800

    支付宝分期还款

--- routes/web.php
+++ routes/web.php
@@ -80,10 +80,15 @@ Route::group(['middleware'=>['auth']],function(){
     Route::get('installments','InstallmentsController@index')->name('installments.index');
     // 分期订单详情
     Route::get('installments/{installment}','InstallmentsController@show')->name('installments.show');
+    Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
+    Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/refund_notify','PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+// 后端回调不能放在 auth 中间件中
+Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
 
 //优惠券
 Route::get('coupon_codes/{code}','CouponCodesController@show')->name('coupon_codes.show');
+
 
