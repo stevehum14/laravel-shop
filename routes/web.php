9e8d03c56c4de76858056d6ee7f1f421edd94f36
洪学臣 <stevehum14@gmail.com>
2021-03-24 11:12:17 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,8 @@ Route::group(['middleware'=>['auth']],function(){
 //    });
     // 众筹商品下单
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
-
+    // 分期支付
+    Route::post('payment/{order}/installment','PaymentController@payByInstallment')->name('payment.installment');
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/refund_notify','PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
