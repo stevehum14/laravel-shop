7512226b2ae604442865d5346482b0dd98ded0d4
stevehum14 <466373766@qq.com>
2021-03-28 22:56:56 +0800

    修复分面搜索 bug

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -70,10 +70,10 @@ abstract class CommonProductsController extends AdminController
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
         });
-//        $form->saved(function(Form $form){
-//            $product = $form->model();
-//            dispatch(new SyncOneProductToES($product));
-//        });
+        $form->saved(function(Form $form){
+            $product = $form->model();
+            dispatch(new SyncOneProductToES($product));
+        });
 
         return $form;
     }
